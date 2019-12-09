6ad650b205cf1adad2891008f1be28afda2e6793
a116acty <57255395+a116acty@users.noreply.github.com>
2019-12-09 22:58:34 +0900

    Delete slackbot_settings.py

--- slackbot_settings.py
+++
@@ -1,6 +0,0 @@
-# coding: utf-8
-# botアカウントのトークン
-API_TOKEN = "xoxb-740901051603-752393495013-eb2Za1aBzjX7fZMne0tjWLUo"
-#API_TOKEN = "xoxb-805156760338-805173633746-GyUjUfsfdd3fRpnimPEUZdno"
-# デフォルトメッセージ
-DEFAULT_REPLY = "Bot応答メッセージ"
